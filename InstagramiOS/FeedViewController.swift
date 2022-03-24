3a46cdc14add7a4a1db476a207a5290a622bdd20
Isaac <tadeleyisake@gmail.com>
2022-03-23 21:20:25 -0400

    Can post to parse + UI of table cell

--- InstagramiOS/FeedViewController.swift
+++ InstagramiOS/FeedViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
